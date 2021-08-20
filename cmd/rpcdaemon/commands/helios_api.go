c3d072adb7a4fe04d5f05d1922297e8f6c1c4fcf
Advaith <advaith@chainist.com>
2021-08-20 10:48:43 -0700

    read txn and block

--- cmd/rpcdaemon/commands/helios_api.go
+++ cmd/rpcdaemon/commands/helios_api.go
@@ -6,6 +6,16 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+
+    "github.com/ledgerwatch/erigon/core",
+    "github.com/ledgerwatch/erigon/core/types",
+    "github.com/ledgerwatch/erigon/core/rawdb",
+    "github.com/ledgerwatch/erigon/ethdb",
+    "github.com/ledgerwatch/erigon/internal/ethapi",
+    "github.com/ledgerwatch/erigon-lib/kv",
+    "github.com/ledgerwatch/erigon/common",
+    "github.com/ledgerwatch/erigon",
+    "github.com/holiman/uint256"
 )
 
 type HeliosAPI interface {
@@ -25,6 +35,32 @@ func NewHeliosAPI(base *BaseAPI, db kv.RoDB) *HeliosAPIImpl {
 	}
 }
 
-func(api *HeliosAPIImpl) GetInternalOperations(ctx context.Context, hash common.Hash) ([]string, error) {
-    fmt.Println("Not implemented!)
+func(api *HeliosAPIImpl) GetInternalOperations(ctx context.Context, hash common.Hash) (string res, error) {
+
+    // begin read only transaction
+    tx, err := api.db.BeginRo(ctx)
+
+    if err != nil {
+        return nil, err
+    }
+
+    defer tx.Rollback()
+
+    txn, blockHash, _, txIndex, err := rawdb.ReadTransaction(tx, hash)
+
+    if err != nil {
+        return err
+    }
+    if txn == nil {
+        return nil, fmt.Errorf("txn: %x not found, txn)
+    }
+
+    block, err := rawdb.ReadBlockByHash(txn, blockHash)
+
+    if err != bil {
+        return nil, err
+    }
+
+    fmt.Println("Found txn: %x", txn)
 }
+
